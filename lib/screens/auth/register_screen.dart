7887cc52e9a3321aa40cc8be887ea13ec8764687
Pritam STP <93328629+Pritam81@users.noreply.github.com>
2025-07-03 18:46:33 +0530

    Register pw visibility added

--- lib/screens/auth/register_screen.dart
+++ lib/screens/auth/register_screen.dart
@@ -76,7 +76,6 @@ class _RegisterScreenState extends State<RegisterScreen> {
     }
   }
   @override
-
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
