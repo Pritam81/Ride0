7f2a08695883c618fbaf80977e9e8d53ac692f0b
Pritam STP <93328629+Pritam81@users.noreply.github.com>
2025-07-11 22:11:40 +0530

    Ride now content file created

---
+++ lib/screens/home/HomeTab/ride_now_content_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class RidenowcontentScreen extends StatefulWidget {
+  const RidenowcontentScreen({super.key});
+
+  @override
+  State<RidenowcontentScreen> createState() => _RidenowcontentScreenState();
+}
+
+class _RidenowcontentScreenState extends State<RidenowcontentScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(backgroundColor: Colors.white);
+  }
+}
