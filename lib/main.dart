4526fda4a4c10c9214471d59959adbcc8f96103a
Pritam STP <93328629+Pritam81@users.noreply.github.com>
2025-07-15 22:29:05 +0530

    Main file updated with routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:rideo/screens/home/ActivityTab/DriverDetails/current_driver_details_screen.dart';
+import 'package:rideo/screens/home/ActivityTab/DriversProfile/past_driver_profile_screen.dart';
 import 'package:rideo/screens/auth/forgot_password_screen.dart';
 import 'package:rideo/screens/auth/login_screen.dart';
 import 'package:rideo/screens/auth/register_screen.dart';
+import 'package:rideo/screens/home/home_screen.dart';
 import 'package:rideo/screens/onboarding_screen.dart';
 import 'package:rideo/screens/splash_screen.dart';
 
@@ -15,11 +18,17 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: const SplashScreen(), routes: {
-      '/register': (context) => const RegisterScreen(),
-      '/onboarding': (context) => const OnboardingScreen(),
-      '/login': (context) => const LoginScreen(),
-      '/forgot-password': (context) => const ForgotPasswordScreen(),
-    });
+    return MaterialApp(
+      home: const SplashScreen(),
+      routes: {
+        '/register': (context) => const RegisterScreen(),
+        '/onboarding': (context) => const OnboardingScreen(),
+        '/login': (context) => const LoginScreen(),
+        '/forgot-password': (context) => const ForgotPasswordScreen(),
+        '/homescreen': (context) => const HomeScreen(),
+        '/past-driver-profile': (context) => const PastDriverProfileScreen(),
+        '/current-driver-details': (context) => const CurrentDriverDetailsScreen(),
+      },
+    );
   }
 }
