52799d79b9854e00949d9a76d5bd1c3f9d353618
Pritam STP <93328629+Pritam81@users.noreply.github.com>
2025-07-28 18:47:58 +0530

    activity screen updated

--- lib/screens/home/ActivityTab/activity_screen.dart
+++ lib/screens/home/ActivityTab/activity_screen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:get/get_connect/http/src/utils/utils.dart';
 import 'package:rideo/screens/home/ActivityTab/current_drives_screen.dart';
 import 'package:rideo/screens/home/ActivityTab/job_history_screen.dart';
 import 'package:rideo/screens/home/ActivityTab/past_driver_screen.dart';
