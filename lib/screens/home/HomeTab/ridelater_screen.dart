eb1119696f8707d6fe6bb8a5bceea6b40e61c2be
Pritam STP <93328629+Pritam81@users.noreply.github.com>
2025-07-28 18:50:05 +0530

    ride later screen created

---
+++ lib/screens/home/HomeTab/ridelater_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class RideLaterScreen extends StatefulWidget {
+  const RideLaterScreen({super.key});
+
+  @override
+  State<RideLaterScreen> createState() => _RideLaterScreenState();
+}
+
+class _RideLaterScreenState extends State<RideLaterScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
