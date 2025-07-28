48ecc5e14acbd2aae51be39ce51e97df0b351b89
Pritam STP <93328629+Pritam81@users.noreply.github.com>
2025-07-28 18:49:09 +0530

    Jop posting screen created

---
+++ lib/screens/home/HomeTab/postajob_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class PostajobScreen extends StatefulWidget {
+  const PostajobScreen({super.key});
+
+  @override
+  State<PostajobScreen> createState() => _PostajobScreenState();
+}
+
+class _PostajobScreenState extends State<PostajobScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
